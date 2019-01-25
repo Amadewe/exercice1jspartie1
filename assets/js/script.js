fc313082bc867bd59d82c8f528c11a9770048e4b
Amadewe <dewevre.amandine@gmail.com>
2019-01-25 13:19:33 +0100

    Ajout des commentaires / vu onMousseOver

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// On déclare la fonction avec comme nom: changeimage
 function changeimage() {
+// on récupère la source de la seconde image
 document.getElementById('image1').src='assets/img/image1_2.jpg';
 }
